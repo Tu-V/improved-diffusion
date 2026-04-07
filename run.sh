dcdb107fa758b59943db60b00967b9deca469141
tu <vutu201130@gmail.com>
2026-04-07 22:17:01 +0700

    4 v100

--- run.sh
+++ run.sh
@@ -5,5 +5,5 @@ DIFFUSION_FLAGS="--diffusion_steps 4000 --noise_schedule linear"
 TRAIN_FLAGS="--lr 1e-4 --batch_size 64 --lr_anneal_steps 125000"
 
 # python scripts/image_train.py --data_dir simple-shapes-5k $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
-NUM_GPUS=1
+NUM_GPUS=4
 mpiexec -n $NUM_GPUS python scripts/image_train.py --data_dir simple-shapes-5k $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
