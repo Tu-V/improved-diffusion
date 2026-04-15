02504b4696227a765f9f3e1ec8e4b67abc030548
tu <vutu201130@gmail.com>
2026-04-15 14:01:05 +0700

    sampling from different checkpoint

--- sample_np_2_pic.py
+++ sample_np_2_pic.py
@@ -1,8 +1,8 @@
 from PIL import Image
 import numpy as np
 
-data = np.load("/Users/admin/workspace/improved-diffusion/simple-shapes-5k-checkpoints-sampling/samples_2000x64x64x3.npz")
+data = np.load("/Users/admin/workspace/improved-diffusion/simple-shapes-5k-checkpoints-10000s-sampling-250steps/samples_2000x64x64x3.npz")
 arr = data["arr_0"]
 
 for i, img in enumerate(arr):
-    Image.fromarray(img).save(f"/Users/admin/workspace/improved-diffusion/simple-shapes-5k-checkpoints-sampling/sample_{i:03d}.png")
+    Image.fromarray(img).save(f"/Users/admin/workspace/improved-diffusion/simple-shapes-5k-checkpoints-10000s-sampling-250steps/sample_{i:03d}.png")
